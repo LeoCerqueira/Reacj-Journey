b7fdc19e35fa26ec93214aac404f1b93ab18773d
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-03-26 09:35:34 -0300

    React

--- src/index.js
+++ src/index.js
@@ -2,29 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const books = [
+	{
+		img:
+			'https://images-eu.ssl-images-amazon.com/images/I/51T2TGvkilL._AC_UL200_SR200,200_.jpg',
+		title: 'Boca de sobrau! U lalau',
+		author: 'Mirial',
+	},
+	{
+		img:
+			'https://images-na.ssl-images-amazon.com/images/I/91p5b0UgbKL._AC_UL200_SR200,200_.jpg',
+		title: 'Weed, the power, the love',
+		author: 'Mirial e Caique',
+	},
+];
+
+const names = ['leo', 'shiva', 'shana'];
+
 function BookList() {
-	return (
-		<section className="booklist">
-			<Book />
-		</section>
-	);
+	return <section className="booklist">{names}</section>;
 }
 
-const author = "Mirial"
-
-const Book = () => {
-	
-	const title = 'Boca de sobrau! U lalau'
+const Book = (props) => {
+	const { img, title, author } = props; //destructuring
 	return (
 		<article className="book">
-			<img
-				src="https://images-eu.ssl-images-amazon.com/images/I/51T2TGvkilL._AC_UL200_SR200,200_.jpg"
-				alt="Book"
-			/>
+			<img src={img} alt="Book" />
 			<h1>{title}</h1>
-
-			<h4>{author.toUpperCase()}</h4>
-			
+			<h4>{author}</h4>
 		</article>
 	);
 };
